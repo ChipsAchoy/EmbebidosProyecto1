2e74ec04628bd915204a28c7fc679cc2bf4b47fc
David De la Hoz <divad0907@gmail.com>
2024-04-01 21:01:56 -0600

    Ya listo falta imagen

--- Webpage/server.py
+++ Webpage/server.py
@@ -1,12 +1,9 @@
 from flask import Flask,render_template,request,redirect,url_for
 from waitress import serve
-from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 
 app=Flask(__name__,static_url_path='/static',static_folder='static')
 
-scheduler=BackgroundScheduler(daemon=True)
-scheduler.start()
 
 users={'admin':'admin'}
 button_states = {'Light1': False, 'Light2': False, 'Light3': False,'Light4': False,'Light5': False}
@@ -14,9 +11,9 @@ door_states={'Door1':False,'Door2':False,'Door3':False,'Door4':False}
 
 def send_request_to_pi(endpoint, data=None):
     try:
-        response = requests.post(f"http://127.0.0.1/embebidos/{endpoint}", json=data)
+        response = requests.post(f"http://127.0.0.1:8777/embebidos/{endpoint}", json=data)
         response.raise_for_status()
-        return response.json()
+        return "OK"
     except requests.exceptions.RequestException as e:
         # Handle request errors
         print(f"Request error: {e}")
@@ -39,21 +36,25 @@ def login():
             return  render_template('login.html',error=error)
     return render_template('login.html')
 
+@app.route('/get_output',methods=['GET'])
 def get_output_data():
     try:
-        response=requests.get('https://127.0.0.1/embebidos/outputs')
+        response=requests.get('http://127.0.0.1:8777/embebidos/inputs')
         response.raise_for_status()
-        data=response.json()
-        print(response)
+        data=response.text
+        print("DATA")
+        print(data)
         i=0
+        data = str(data)
+        #door_array = ["Door1","Door2","Door3","Door4"]
         for key in door_states.keys():
-            if(data[i]==0):
+            if(data[i]=='0'):
                 door_states[key]=False
             else:
                 door_states[key]=True
             i+=1
-
-        return data, 200
+        print(door_states)
+        return redirect('/index')
     except requests.exceptions.RequestException as e:
         print(f"Error processing data: {e}")
 
@@ -72,13 +73,12 @@ def toggle_button(button_id):
             outputs+="1"
     data = {"outputs":outputs}
     print(data)
-    send_request_to_pi("inputs", data=data)
+    send_request_to_pi("outputs", data=data)
 
     # Redirect back to the home page
     return redirect('/index')
 
 
-scheduler.add_job(get_output_data, 'interval', seconds=10)
 
 if __name__=="__main__":
     serve(app,host="0.0.0.0",port=5000)
---
+++ project1emb/main_fin.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Inicio de mi_programa
+
+# Inicia mi_programa desde la raíz del sistema
+/main
+
+exit 0
+
